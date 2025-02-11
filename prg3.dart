c01956d19a701d6b62127337bcb121279bcc77d3
Isha Jagdale <ishajagdale07@gmail.com>
2025-02-11 18:53:14 +0530

    basic dart codes

---
+++ prg3.dart
@@ -0,0 +1,5 @@
+void main() {
+  print(4);
+  print(4 + 3);
+  print(4 / 2);
+}
---
+++ prg4.dart
@@ -0,0 +1,11 @@
+void main() {
+  int score = 23;
+  double per = 93.2;
+  String name = "isha";
+  bool isValid = true;
+
+  print(score);
+  print(per);
+  print(name);
+  print(isValid);
+}
